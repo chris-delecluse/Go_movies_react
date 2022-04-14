65670c03911f1e9fd1c8eb940ed0079d5c3ad408
Christopher Delecluse <christopherdeleclusepro@gmail.com>
2022-04-14 16:12:25 +0200

    modify Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="footer">
-
+            <p>
+                Project for learning react, code :
+                <span><a href="https://github.com/chris-delecluse/Go_movies_react" target="_blank"> click here</a></span>
+            </p>
         </div>
     )
 }
